b452a78bd08fcd5630263d788f8ef8f90f9a6e6e
Aashish Sharma <aasyspoudyal@hotmail.com>
2018-12-27 08:31:41 -0600

    crearted the function to read the text area value and created the remove button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 //variables
-
+const listList= document.getElementById('List-list');
 
 //Event Listeners
 eventListeners();
@@ -13,6 +13,25 @@ document.querySelector('#form').addEventListener('submit',newList);
 //Function
 function newList(e){
     e.preventDefault();
-    console.log('Form Submitted')
+   
+    //Read the textarea value
+    const List = document.getElementById('List').value;
+    //create the remove button
+    const removebtn =document.createElement('a');
+    removebtn.classList='remove-List';
+    removebtn.textContent='X';
+
+    //Create a <li> element
+    const li =document.createElement('li');
+    li.textContent=List;
+    
+
+    //Add the remove button to each tweet
+    li.appendChild(removebtn);
+
+    //Add to the list
+    listList.appendChild(li);
+
+
 
 }
