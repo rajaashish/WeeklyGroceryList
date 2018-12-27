8863dcdbdb9cac1c056bf6324587cb110571f940
Aashish Sharma <aasyspoudyal@hotmail.com>
2018-12-27 09:11:36 -0600

    nopw the items can be saved in the local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,127 @@
-//variables
-const listList= document.getElementById('List-list');
+// Variables
+const ListList = document.getElementById('List-list');
 
-//Event Listeners
+
+// Event Listeners
 eventListeners();
 
-function eventListeners(){
+function eventListeners() {
+     // Form Submission
+     document.querySelector('#form').addEventListener('submit', newList);
 
-//Form submission
-document.querySelector('#form').addEventListener('submit',newList);
+     // Remove List from the list
+     ListList.addEventListener('click', removeList);
 
-//remove from the list
-listList.addEventListener('click',removeList)
+     // Document
+     document.addEventListener('DOMContentLoaded', localStorageOnLoad);
 }
 
-//Function
-function newList(e){
-    e.preventDefault();
-   
-    //Read the textarea value
-    const List = document.getElementById('List').value;
-    //create the remove button
-    const removebtn =document.createElement('a');
-    removebtn.classList='remove-List';
-    removebtn.textContent='X';
 
-    //Create a <li> element
-    const li =document.createElement('li');
-    li.textContent=List;
-    
+// Functions
 
-    //Add the remove button to each tweet
-    li.appendChild(removebtn);
+function newList(e) {
+     e.preventDefault();
 
-    //Add to the list
-    listList.appendChild(li);
+     // Read the textarea value
+     const List = document.getElementById('List').value;
 
-    //remove the list from the DoM
-    function removeList(e){
-        if(e.target.classList.contains('remove-List')){
-            e.target.parentElement.remove();
-        } 
-    }
+     // Create the remove button
+     const removeBtn = document.createElement('a');
+     removeBtn.classList = 'remove-List';
+     removeBtn.textContent = 'X';
 
+     // Create an <li> element
+     const li = document.createElement('li');
+     li.textContent = List; 
+     
+     // Add the remove button to each List
+     li.appendChild(removeBtn);
 
+     // Add to the list
+     ListList.appendChild(li);
 
+     // add to Local Storage
+     addListLocalStorage(List);
+
+     // Print the alert
+     alert('List Added');
+
+     this.reset();
+} 
+
+// Removes the Lists from the DOM
+function removeList(e) {
+     if(e.target.classList.contains('remove-List')) {
+          e.target.parentElement.remove();
+     } 
+
+     // Remove from Storage
+      removeListLocalStorage( e.target.parentElement.textContent ) ;
+}
+
+// Adds the Lists into the local storage
+function addListLocalStorage(List) {
+     let Lists = getListsFromStorage();
+
+     // Add the List into the array
+     Lists.push(List);
+
+     // Convert List array into String
+     localStorage.setItem('Lists', JSON.stringify( Lists )  );
+}
+
+function getListsFromStorage() {
+     let Lists;
+     const ListsLS = localStorage.getItem('Lists');
+     // Get the values, if null is returned then we create an empty array
+     if(ListsLS === null) {
+          Lists = [];
+     } else {
+          Lists = JSON.parse( ListsLS ); 
+     }
+     return Lists;
+}
+
+// Prints Local Storage Lists on Load
+function localStorageOnLoad() {
+     let Lists = getListsFromStorage();
+
+     // Loop throught storage and then print the values
+     Lists.forEach(function(List) {
+          // Create the remove button
+          const removeBtn = document.createElement('a');
+          removeBtn.classList = 'remove-List';
+          removeBtn.textContent = 'X';
+
+          // Create an <li> element
+          const li = document.createElement('li');
+          li.textContent = List; 
+          
+          // Add the remove button to each List
+          li.appendChild(removeBtn);
+
+          // Add to the list
+          ListList.appendChild(li);
+     });
+}
+
+// Removes the List from local storage
+
+function removeListLocalStorage(List) {
+     // get Lists from storage
+     let Lists = getListsFromStorage();
+
+     // Remove the X from the List
+
+     const ListDelete = List.substring( 0, List.length -1 );
+
+     // Loop Throught  the Lists and remove the List that's equal
+     Lists.forEach(function(ListLS, index) {
+          if(ListDelete === ListLS) {
+               Lists.splice(index, 1);
+          }
+     });
+
+     // Save the data 
+     localStorage.setItem('Lists', JSON.stringify(Lists) );
 }
